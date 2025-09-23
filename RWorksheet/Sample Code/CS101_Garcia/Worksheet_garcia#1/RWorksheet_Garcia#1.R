991e78b79e5f1f09e3be0793ef65991234f791b5
Carol Garcia <carol.garcia@students.isatu.edu.ph>
2025-09-23 05:14:03 +0000

    Added file worksheet#1

--- RWorksheet/Sample Code/CS101_Garcia/Worksheet_garcia#1/RWorksheet_Garcia#1.R
+++ RWorksheet/Sample Code/CS101_Garcia/Worksheet_garcia#1/RWorksheet_Garcia#1.R
@@ -41,3 +41,5 @@ name <- readline(prompt = "Input your name: ")
 age <- readline(prompt = "Input your age: ")
 print(paste("My name is", name, "and I am", age, "years old."))
 print(R.version.string)
+
+
